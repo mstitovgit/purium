31dc0f3951f2c160331763e7ecf16dc6759d9509
Максим Титов <mstitov@MacBook-Air-Maksim-3.local>
2024-03-15 22:35:27 +0300

    Fixes

--- src/partials/S/S_ArticleModules.js
+++ src/partials/S/S_ArticleModules.js
@@ -4,7 +4,6 @@ import { getArticlesData } from '../../database.js'
 let article
 
 if (document.getElementById('ArticlesModules')) {
-    console.log('this is article');
 getArticlesData().then((array) => {
   SetArticle(array)
   RenderArticle()
@@ -15,15 +14,19 @@ function SetArticle(articles) {
   articles.forEach((element) => {
     if (element.id == currentarticleid) {
       article = element
-      console.log('set article', article);
     }
   })
 }
 
 function RenderArticle() {
-  console.log('render', article.name)
+  console.log('render', article.published)
+  let date = new Date (article.published)
+  console.log(date)
+  let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
   document.querySelector('.A_ArticleName').textContent = article.name
-  document.querySelector('.A_ArticlePublicationInfo').textContent = ('опубликовано '+ article.published)
+  document.querySelector('.A_ArticlePublicationInfo').textContent = ('опубликовано '+ day +"/" + month +"/" + year)
   let newElement = document.createElement('div')
   newElement.classList ='O_ArticleText'
     newElement.innerHTML = article.text
